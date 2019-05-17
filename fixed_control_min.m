2a2e7e6eb127bcca804fcd58f82538eadd2d6374
erez <ekrimsky@stanford.edu>
2019-05-16 22:51:25 -0700

    uses X_mid now

--- fixed_control_min.m
+++ fixed_control_min.m
@@ -9,22 +9,38 @@
 %****************************
 
 
+
+
+
 % Compute X in the calling function with X = U*D --> spring positions at every step 
 
 % X is n x (h + 1)
 
 
-function [b, k] = fixed_control_min(f_des, U, X, b_init, k_init)
 
 
 
-X_mid = (X(:, 1:end-1) - X(:, 2:end))/2;	% diff may be faster here 
+
+% function [b, k, f_mids_out] = fixed_control_min(f_des, U, X, b_init, k_init)
+%X_mid = (X(:, 1:end-1) - X(:, 2:end))/2;	% diff may be faster here 
+function [b, k, f_mids_out] = fixed_control_min(f_des, U, X_mid, b_init, k_init)
+
+
+% enforce f and k to be col vecs 
+b_init = b_init(:); 
+k_init = k_init(:); 
+
+
+
 
 
 
 % Comment this out but good sanity check for now 
 f_init = (b_init' * U) + k_init' * (U .* X_mid); 
 
+
+%display(f_des)
+%display(f_init)
 obj_init = norm(f_des - f_init);
 
 display(obj_init); 
@@ -59,8 +75,14 @@ cvx_begin quiet
 
 cvx_end 
 
-display(f)
+% display(f)
 
 display(cvx_status)
 display(cvx_optval)
 
+
+
+f_mids_out = (b' * U) + (k' * (U .* X_mid)); 
+
+
+f_out = (b' * U) + (k' * (U .* X_mid)); 
